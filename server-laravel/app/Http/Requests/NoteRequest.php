611c361e10bc354a6c70bb893466593e07b0ef1f
Rizuu <hoquoctien0811@gmail.com>
2026-05-17 22:17:30 +0700

    fix: resolve merge conflict in NoteRequest.php

--- server-laravel/app/Http/Requests/NoteRequest.php
+++ server-laravel/app/Http/Requests/NoteRequest.php
@@ -15,12 +15,9 @@ class NoteRequest extends FormRequest
             'content'    => 'nullable|string',
             'label_ids'  => 'nullable|array',
             'label_ids.*'=> 'integer|exists:labels,id',
-<<<<<<< HEAD
             'created_at' => 'nullable|date',
             'visibility' => 'nullable|in:private,link,public',
             'password'   => 'nullable|string|min:4',
-=======
->>>>>>> a518c7f15ee7892eb351a53417168a339bed928d
         ];
     }
 
